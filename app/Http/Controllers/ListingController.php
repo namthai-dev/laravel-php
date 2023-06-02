77097a9f7a870ebde6344b946074de8994adef25
Nam Thai <nam.thai.dev@outlook.com>
2023-06-02 23:46:46 +0700

    Flash message

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -34,7 +34,7 @@ class ListingController extends Controller
 
         Listing::create($formFields);
 
-        return redirect('/');    
+        return redirect('/')->with('message', 'Listing created successfully!');    
     }
 
     // Show single listing
