6ce598b323602c21a3efab9475d2df9c903c017f
Nam Thai <nam.thai.dev@outlook.com>
2023-06-02 22:20:43 +0700

    Update Listing.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,10 @@ class Listing extends Model
 {
     use HasFactory;
 
+    // The naming of the 'scopeFilter' method and calling it using 'filter' matter. 
+    // The 'scope' prefix is meaningful to Laravel and the 'Filter' part can be anything, 
+    // as long as you use the same name in your Controller when calling the method. In other words, if you name the method scopeTry, 
+    // then call it using  ...latest()->try(request(...
     public function scopeFilter($query, array $filters) {
         if ($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request('tag') . '%');
