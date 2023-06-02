664ba597e3871dd6eb8485947645fa8b061756d1
Nam Thai <nam.thai.dev@outlook.com>
2023-06-03 02:29:53 +0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,9 @@ Route::post('/users', [UserController::class, 'store']);
 
 // Log User Out
 Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
